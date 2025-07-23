380e4930b1b0e38b1005b5881acbba7fd5f3220e
Zeel-DV <tatvasoft.pci141@outlook.com>
2025-07-23 15:26:17 +0530

    to make changes in generic repository methods.

--- QuizVerse.Infrastructure/Interface/IGenericRepository.cs
+++ QuizVerse.Infrastructure/Interface/IGenericRepository.cs
@@ -8,11 +8,13 @@ public interface IGenericRepository<T> where T : class
 
     Task<T?> GetAsync(Expression<Func<T, bool>> expression);
 
-    Task<IEnumerable<T>> GetAllAsync();
+    Task<List<T>> GetAllAsync();
 
     Task<List<T>> FindAsync(Expression<Func<T, bool>> expression);
 
     Task AddAsync(T entity);
+
+    Task AddRangeAsync(List<T> entities);
 
     Task UpdateAsync(T entity);
 
--- QuizVerse.Infrastructure/Repository/GenericRepository.cs
+++ QuizVerse.Infrastructure/Repository/GenericRepository.cs
@@ -5,16 +5,9 @@ using QuizVerse.Infrastructure.Interface;
 
 namespace QuizVerse.Infrastructure.Repository;
 
-public class GenericRepository<T> : IGenericRepository<T>
+public class GenericRepository<T>(QuizVerseDbContext _context) : IGenericRepository<T>
            where T : class
 {
-    private readonly QuizVerseDbContext _context;
-
-    public GenericRepository(QuizVerseDbContext _context)
-    {
-        this._context = _context;
-    }
-
     public async Task<bool> Exists(Expression<Func<T, bool>> expression)
     {
         return await _context.Set<T>().AsNoTracking().Where(expression).AnyAsync();
@@ -25,7 +18,7 @@ public class GenericRepository<T> : IGenericRepository<T>
         return await _context.Set<T>().AsNoTracking().Where(expression).FirstOrDefaultAsync().ConfigureAwait(false);
     }
 
-    public async Task<IEnumerable<T>> GetAllAsync()
+    public async Task<List<T>> GetAllAsync()
     {
         return await _context.Set<T>().AsNoTracking().ToListAsync();
     }
@@ -37,13 +30,18 @@ public class GenericRepository<T> : IGenericRepository<T>
 
     public async Task AddAsync(T entity)
     {
-        _context.Set<T>().Add(entity);
+        await _context.Set<T>().AddAsync(entity);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task AddRangeAsync(List<T> entities)
+    {
+        await _context.Set<T>().AddRangeAsync(entities);
         await _context.SaveChangesAsync();
     }
 
     public async Task UpdateAsync(T entity)
     {
-        _context.Entry(entity).State = EntityState.Detached;
         _context.Set<T>().Update(entity);
         await _context.SaveChangesAsync();
     }
