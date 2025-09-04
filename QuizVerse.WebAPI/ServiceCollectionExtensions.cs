ed6f7d228f1087d9678e4f8862cd7cfcf0318ec3
vivekkotadiya17 <tatva.pci135@outlook.com>
2025-09-04 11:54:17 +0530

    to create folder structure for quiz

--- QuizVerse.WebAPI/ServiceCollectionExtensions.cs
+++ QuizVerse.WebAPI/ServiceCollectionExtensions.cs
@@ -31,6 +31,7 @@ public static class ServiceCollectionExtensions
         services.AddScoped<ILeaderboardService, LeaderboardService>();
         services.AddScoped<IUserProfileService, UserProfileService>();
         services.AddScoped<IPlatformConfigurationService, PlatformConfigurationService>();
+        services.AddScoped<IQuizService, QuizService>();
 
         //mappers
         services.AddAutoMapper(typeof(MappingProfile));
