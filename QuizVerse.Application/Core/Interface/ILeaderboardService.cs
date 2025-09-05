eea0986c96de8f43f57c24aba63d677e19f72c2c
Darsh-Tatvasoft <tatva.pca195@outlook.com>
2025-09-05 14:04:44 +0530

    Remaining 3 leader boards complete

--- QuizVerse.Application/Core/Interface/ILeaderboardService.cs
+++ QuizVerse.Application/Core/Interface/ILeaderboardService.cs
@@ -6,4 +6,7 @@ public interface ILeaderboardService
 {
     public Task<UserPerformanceResponseDto> GetUserLeaderboardStats();
     public Task<List<LeaderboardGlobalRankingResponseDto>> GetLeaderboardGlobalRanking();
+    public Task<List<WeeklyLeaderBoardResponseDto>> GetWeeklyLeaderboardRanking();
+    public Task<List<CategoryWiseLeaderBoardResponseDto>> GetQuizCategoryWiseLeaderboardRanking(int categoryId);
+    public Task<List<MonthlyChampionsResponseDto>> GetMonthlyChampions(int month, int year);
 }
